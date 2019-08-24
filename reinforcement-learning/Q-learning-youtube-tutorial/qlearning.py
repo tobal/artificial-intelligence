d28e4f51ab920f9d457d216ec7f0629cefac2031
Tobal <tobal17@gmail.com>
2019-08-24 15:14:08 +0200

    reinforcement/Q-learning/qlearning.py: add parameter constants
    Signed-off-by: Tobal <tobal17@gmail.com>

--- reinforcement-learning/Q-learning-youtube-tutorial/qlearning.py
+++ reinforcement-learning/Q-learning-youtube-tutorial/qlearning.py
@@ -4,6 +4,10 @@ import numpy as np
 env = gym.make('MountainCar-v0')
 env.reset()
 
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+EPISODES = 25000
+
 DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
 
